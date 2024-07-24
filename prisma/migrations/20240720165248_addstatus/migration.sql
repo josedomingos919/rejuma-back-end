87a08c347c097328d2f0aeac771b3f09345f3155
josedomingos919 <josedomingos919@gmail.com>
2024-07-24 09:44:03 +0100

    feat: remove migration

--- prisma/migrations/20240720165248_addstatus/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `schoolyear` ADD COLUMN `statusId` INTEGER NULL,
-    MODIFY `year` VARCHAR(191) NOT NULL;
-
--- AddForeignKey
-ALTER TABLE `schoolyear` ADD CONSTRAINT `schoolyear_statusId_fkey` FOREIGN KEY (`statusId`) REFERENCES `status`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
