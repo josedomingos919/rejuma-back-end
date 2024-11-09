c2a09e4c2e963810b8b34f54cf2724be41db597f
josedomingos919 <josedomingos919@gmail.com>
2024-11-09 07:52:14 +0000

    feat: enable cross

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://gammadc-gm.netlify.app',
+    credentials: true, 
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
